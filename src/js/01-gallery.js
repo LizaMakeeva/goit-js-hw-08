c18e876e832cbdc6a8a59c9c70f0ace6763de752
LizaMakeeva <123816991+LizaMakeeva@users.noreply.github.com>
2023-06-22 12:57:38 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from '/node_modules/simplelightbox';
+// Add imports above this line
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryAdd = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`
+  )
+  .join('');
+galleryEl.innerHTML = galleryAdd;
+galleryEl.style.listStyle = 'none';
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
